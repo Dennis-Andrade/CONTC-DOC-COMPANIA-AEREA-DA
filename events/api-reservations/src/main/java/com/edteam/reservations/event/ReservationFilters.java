77be9dcf2046be5694986ad819f4db5b3fbcc889
Dennis Andrade <denisandradeg@gmail.com>
2026-07-10 15:49:14 -0500

    accion al procesar

--- events/api-reservations/src/main/java/com/edteam/reservations/event/ReservationFilters.java
+++ events/api-reservations/src/main/java/com/edteam/reservations/event/ReservationFilters.java
@@ -1,6 +1,7 @@
 package com.edteam.reservations.event;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public final class ReservationFilters {
@@ -11,4 +12,7 @@ public final class ReservationFilters {
     public static final Predicate<ReservationEvent> VALID_RESERVATION = event -> Objects.nonNull(event)
             && Objects.nonNull(event.getPrice()) && event.getPrice() > 0 && Objects.nonNull(event.getEmails())
             && !event.getEmails().isEmpty();
+
+    public static final Consumer<ReservationEvent> PRINT_PROCESSED_EVENT = event -> System.out
+            .println("Reserva procesada: " + event.getId() + " - " + event.getPassengerName());
 }
